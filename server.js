1a177cc2a98cf9479a05adaf057081aee057c1ef
yafomin11-max <yafomin11@gmail.com>
2026-03-26 17:58:07 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,41 +5,23 @@ const io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
 
-// Хранилище доски: ключ "x,y", значение "color"
 let board = {}; 
 let onlineCount = 0;
 
 io.on('connection', (socket) => {
     onlineCount++;
     io.emit('userCount', onlineCount);
+    socket.emit('initBoard', board);
 
-    // Отправляем текущее состояние доски новому игроку
-    socket.emit('initBoard', Object.entries(board));
-
-    // Одиночный пиксель
     socket.on('drawPixel', (data) => {
         const { x, y, color } = data;
-        if (x >= 0 && x < 1000 && y >= 0 && y < 1000) {
+        // ТЕПЕРЬ ПРОВЕРКА ДО 2000
+        if (x >= 0 && x < 2000 && y >= 0 && y < 2000) {
             board[`${x},${y}`] = color;
             io.emit('updatePixel', { x, y, color });
         }
     });
 
-    // ЦЕЛАЯ ФИГУРА (Линия, Квадрат, Круг)
-    socket.on('drawShape', (data) => {
-        const { pixels, color } = data;
-        if (Array.isArray(pixels)) {
-            pixels.forEach(p => {
-                if (p.x >= 0 && p.x < 1000 && p.y >= 0 && p.y < 1000) {
-                    board[`${p.x},${p.y}`] = color;
-                }
-            });
-            // Рассылаем всем готовую фигуру
-            io.emit('updateShape', { pixels, color });
-        }
-    });
-
-    // Чат
     socket.on('sendMessage', (data) => {
         if (data.text) {
             io.emit('receiveMessage', {
@@ -56,6 +38,4 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => {
-    console.log(`Сервер запущен на порту ${PORT}`);
-});
+http.listen(PORT, () => console.log(`Сервер: http://localhost:${PORT}`));
